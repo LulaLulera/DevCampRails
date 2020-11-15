38f3998e34bec80ad2bdf37f1403925e443822f6
lulalulera <laublaca@gmail.com>
2020-11-15 11:06:13 +0100

    Thechnology add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_091119) do
+ActiveRecord::Schema.define(version: 2020_11_15_094445) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -60,6 +60,14 @@ ActiveRecord::Schema.define(version: 2020_11_15_091119) do
     t.text "badge"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.integer "portfolio_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
@@ -74,4 +82,5 @@ ActiveRecord::Schema.define(version: 2020_11_15_091119) do
   end
 
   add_foreign_key "blogs", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
