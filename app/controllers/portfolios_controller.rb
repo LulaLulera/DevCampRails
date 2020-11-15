8521605beaf1a96d5a8b214643e588beabdf1e34
lulalulera <laublaca@gmail.com>
2020-11-15 12:50:35 +0100

    Implemented nested attributes at view

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,16 @@
 class PortfoliosController < ApplicationController
-
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  
   def index
     @portfolio_items = Portfolio.all
   end
 
   def new
     @portfolio_item = Portfolio.new
+
+    # esto instancia 3 versiones de este portfolio_item, y crea 3 tipos de technologies, es decir, en el form
+    #habrá 3 elementos para añadir tecnologías
+    3.times { @portfolio_item.technologies.build } 
   end
 
   def create
@@ -24,7 +29,11 @@ class PortfoliosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attribute: [:name])
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 
 end
