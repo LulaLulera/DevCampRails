c1a98378a5f649e56a1b1a5fe89208af3d519745
lulalulera <laublaca@gmail.com>
2020-11-16 10:34:39 +0100

    Virtual attribute complete_name user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable (para cuando es necesario confirmar que es un humano, recibe un email de confirmación), 
-  # :lockable (poder bloquear un usuario, número de veces que intenta acceder),
-  # :timeoutable (tiempo en el que se desloguea aunque esté en la app),  and 
-  # :omniauthable (integra otros logueos como por ejemplo facebook o google)
-  devise :database_authenticatable, :registerable,
+	  # Include default devise modules. Others available are:
+	  # :confirmable (para cuando es necesario confirmar que es un humano, recibe un email de confirmación), 
+	  # :lockable (poder bloquear un usuario, número de veces que intenta acceder),
+	  # :timeoutable (tiempo en el que se desloguea aunque esté en la app),  and 
+	  # :omniauthable (integra otros logueos como por ejemplo facebook o google)
+	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    validates_presence_of :name, :surname
+
+
+    #Esto va a crear un atributo virtual "complete_name"
+    def complete_name
+     	self.name.to_s + " " + self.surname.to_s 
+    end
+
 end
