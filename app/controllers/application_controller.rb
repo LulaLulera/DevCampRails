cb63379801d4a376037f032f70c053a2ee375520
lulalulera <laublaca@gmail.com>
2020-11-16 17:19:10 +0100

    IMplement Null Object Pattern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,6 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	include DeviseWhitelist
 	include SetSource
+	include CurrentUserConcern
 
-	def current_user #sobreescribimos el método current_user, asi evitamos tener que poner condicionales en las vistas por ejemplo con un guest user
-		#Si exite current_user, el comprtamiento es el mismo que si no hubieramos sobreescrito el método
-		#en caso contrario utilizamos el Null Object Pattern, para ello usamos "OpenStruct"
-		#El tipo de clase de current_user solo puede ser de tipo User o de tipo OpenStruct para poder hacer la distinción entre ambos
-		super || OpenStruct.new(complete_name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@guest.es")
-
-	end
 end 
