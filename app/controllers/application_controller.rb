e0288795b2392293aa8f656cc7a6d6c347c6a3f7
lulalulera <laublaca@gmail.com>
2020-11-16 13:35:09 +0100

    Add concern to setSource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	include DeviseWhitelist
-	before_action :set_source
-
-	#ESte método nos permite acceder a los parametros de sesion que venga por ejemplo de otra página
-	def set_source
-		session[:source] = params[:q] if params[:q]
-	end
+	include SetSource
 end 
